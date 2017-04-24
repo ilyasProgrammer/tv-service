6aa11e748da6accfff3d0d34d710734bbeee6248
ilyasprogrammer <rakhimkulov@it-projects.info>
2017-04-24 13:31:56 +0500

    imp partner view

--- tv_channel/models.py
+++ tv_channel/models.py
@@ -13,5 +13,26 @@ class TVChannel(models.Model):
                               ('movie', 'Movie'),
                               ('games', 'Games'),
                               ('news', 'News')], 'Genre')
-    type = fields.Selection([('1', '1'), ('2', '2')], 'Type')
+    type = fields.Selection([('1', '1'),
+                             ('2', '2'),
+                             ('3', '3')], 'Channel Type')
     format = fields.Selection([('pal', 'PAL'), ('ntsc', 'NTSC')], 'Format')
+
+
+class PartnerTVChannels(models.Model):
+    _inherit = 'res.partner'
+
+    channel_ids = fields.Many2many('tv.channel')
+    service_type = fields.Selection([('direct_reception', 'Direct reception'),
+                                     ('IPTV', 'IPTV'),
+                                     ('OTT', 'OTT')], 'Service Type')
+    customer_type = fields.Selection([('hotel', 'Hotel'),
+                             ('resort', 'Resort'),
+                             ('hospital', 'Hospital'),
+                             ('clinic', 'Clinic'),
+                             ('IPTV', 'IPTV operator'),
+                             ('OTT', 'OTT operator'),
+                             ('compound', 'Compound '),
+                             ('private_enterprise', 'Private enterprise'),
+                             ('public_organization', 'Public organization'),
+                             ('MDU', 'MDU')], 'Customer Type', default='hotel')
