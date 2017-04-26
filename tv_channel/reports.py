ad9624815465762d6b7d58b2c86e4dcac54e2e1e
ilyasprogrammer <rakhimkulov@it-projects.info>
2017-04-26 22:56:10 +0500

    cool report model

--- tv_channel/reports.py
+++ tv_channel/reports.py
@@ -7,19 +7,41 @@ from openerp import models, fields
 class ChannelsVSPartners(models.Model):
     _name = "channels.vs.partners"
     _auto = False
-    channel = fields.Many2one('tv.channel', string='channel', readonly=True)
-    language = fields.Many2one('res.lang', string='language', readonly=True)
-    partner = fields.Many2one('res.partner', string='partner', readonly=True)
-    format = fields.Boolean(string='format', readonly=True)
-    # partner = fields.Char(string='partner', readonly=True)
+    channel = fields.Many2one('tv.channel', string='Channel', readonly=True)
+    partner = fields.Char(string='Partners', readonly=True)
+    qty = fields.Integer(string='Quantity', readonly=True)
+    language = fields.Many2one('res.lang', 'Language')
+    genre = fields.Char(string='Genre')
+    type = fields.Char(string='Type')
+    format = fields.Char(string='Format')
+    technology = fields.Char(string='Technology')
+    country = fields.Char(string='Country')
 
     def init(self, cr):
         tools.drop_view_if_exists(cr, 'channels_vs_partners')
         cr.execute("""
             create or replace view channels_vs_partners as (
-            SELECT t.language, t.format, r.tv_channel_id as id, r.tv_channel_id as channel, p.id as partner
+            SELECT 
+                r.tv_channel_id as id, 
+                r.tv_channel_id as channel, 
+                string_agg(p.name, ', ') as partner , 
+                COUNT(*) as qty,
+                t.language,
+                t.genre,
+                t.type,
+                t.format,
+                t.technology,
+                c.name as country
             FROM res_partner_tv_channel_rel as r
             LEFT JOIN res_partner p ON (r.res_partner_id = p.id)
             LEFT JOIN tv_channel t ON (r.tv_channel_id = t.id)
+            LEFT JOIN res_country c ON (t.country = c.id)
+            group by r.tv_channel_id, t.language,
+                t.genre,
+                t.type,
+                t.format,
+                t.technology,
+                c.name
+            order by id
             )
         """)
