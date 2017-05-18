aef40afe0b45b85de45aff0e524e00926c136aba
ilyasprogrammer <rakhimkulov@it-projects.info>
2017-05-18 21:30:55 +0500

    other access

--- modules/tv_access/__openerp__.py
+++ modules/tv_access/__openerp__.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "tv_access",
-    'summary': """tv_access""",
+    'summary': """acces groups and rules for all needed models""",
     'description': """tv_access""",
     'author': "Ilyas",
     'website': "https://github.com/ilyasProgrammer",
     'category': 'Custom',
     'version': '1.1',
-    'depends': ['tv_channel'],
+    'depends': ['tv_channel',
+                'sale',
+                'project',
+                'purchase',
+                'hr',
+                ],
     'external_dependencies': {},
     'data': [
         'security/tv_security.xml',
